d928d49752383a417861391995ee1c0579121692
2rv <turaievkyrylo@gmail.com>
2023-09-10 11:48:57 +0300

    init

---
+++ back/src/route/index.js
@@ -0,0 +1,18 @@
+// Підключаємо роутер до бек-енду
+const express = require('express')
+const router = express.Router()
+
+// Підключіть файли роутів
+// const test = require('./test')
+// Підключіть інші файли роутів, якщо є
+
+// Об'єднайте файли роутів за потреби
+// router.use('/', test)
+// Використовуйте інші файли роутів, якщо є
+
+router.get('/', (req, res) => {
+  res.status(200).json('Hello World')
+})
+
+// Експортуємо глобальний роутер
+module.exports = router
